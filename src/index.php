bba25e95098ee1f1c29e04eaed6b046023da2a2b
Sasha <lonagi22@gmail.com>
2020-12-07 19:57:47 +0200

    type effect

--- src/index.php
+++ src/index.php
@@ -3,11 +3,21 @@
 <head>
 	<title>README gif</title>
 	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<script src="https://unpkg.com/typewriter-effect@latest/dist/core.js"></script>
 	<style type="text/css">
 		#particles-js {
     background-color: #0C0C0C;
     height: 860px;
     width: 100%;
+}
+#typewriter {
+	color:white;
+    position: absolute;
+    font-size: 60px;
+    font-family: 'B612', sans-serif;
+    font-weight: 700;
+    width: 800px;
+    margin: 350px 250px;
 }
 	</style>
 </head>
@@ -21,5 +31,28 @@
 	particlesJS.load('particles-js', 'particlesjs-config.php', function() {
   console.log('callback - particles.js config loaded');
 });
+	const instance = new Typewriter('#typewriter', {
+    loop: true
+});
+	instance.typeString("Hello World!")
+    .pauseFor(500)
+    .deleteAll()
+    .typeString('I am Sasha')
+    .pauseFor(1000)
+    .deleteChars(15)
+    .typeString('CEO & Lead Dev.<br>New Vektor Group')
+    .pauseFor(1000)
+    .deleteChars(16)
+    .typeString('DronesC')
+    .pauseFor(1000)
+    .deleteAll()
+    .typeString('Currently learning<br>Neurobiology')
+    .pauseFor(2000)
+    .deleteAll()
+    .typeString('Contact me<br>facebook.com/lonagi22')
+    .pauseFor(2000)
+    .deleteAll()
+    .start();
+</script>
 </body>
 </html>
