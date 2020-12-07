409b60a8b60515b5169202266cceb104c9d33fae
Sasha <lonagi22@gmail.com>
2020-12-07 19:57:37 +0200

    particles

--- src/index.php
+++ src/index.php
@@ -2,10 +2,24 @@
 <html>
 <head>
 	<title>README gif</title>
+	<script src="https://cdn.jsdelivr.net/particles.js/2.0.0/particles.min.js"></script>
+	<style type="text/css">
+		#particles-js {
+    background-color: #0C0C0C;
+    height: 860px;
+    width: 100%;
+}
+	</style>
 </head>
 <body>
 <div id="particles-js">
         <div id="typewriter"></div>
 </div>
+
+
+<script type="text/javascript">
+	particlesJS.load('particles-js', 'particlesjs-config.php', function() {
+  console.log('callback - particles.js config loaded');
+});
 </body>
 </html>
